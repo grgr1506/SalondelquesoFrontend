45de67efacdd39ea1d421290541110871682ae74
Gonzalo <galindoramosgonzalo@gmail.com>
2026-02-26 14:55:21 -0500

    26/02 14:55

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     // CAMBIA LA IP POR LA QUE ANOTASTE EN EL PASO 1
-    baseURL: 'http://192.168.100.128:3000/api', 
+    baseURL: 'https://salondelquesobackend.onrender.com/api', 
 });
 
 export default api;
