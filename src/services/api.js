6644ddda8ffd726aa0809d424306f89b878bcc6f
grgr1506 <galindoramosgonzalo@gmail.com>
2026-05-11 13:02:07 -0500

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -5,4 +5,20 @@ const api = axios.create({
     baseURL: 'https://salondelquesobackend.onrender.com/api', 
 });
 
+// Interceptor para inyectar el token de seguridad automáticamente en cada petición
+api.interceptors.request.use((config) => {
+    const usuarioString = localStorage.getItem('usuario');
+    
+    if (usuarioString) {
+        const usuario = JSON.parse(usuarioString);
+        // Si el usuario tiene un token guardado, lo ponemos en la cabecera
+        if (usuario.token) {
+            config.headers.Authorization = `Bearer ${usuario.token}`;
+        }
+    }
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
+
 export default api;
